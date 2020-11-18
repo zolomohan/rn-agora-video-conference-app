813ccddc68271bf53167c3fda29e56c60611cda1
zolomohan <mohanzolomon@gmail.com>
2020-11-19 03:34:42 +0530

    Add End Call Go Back to Home Screen

--- screens/Conference.js
+++ screens/Conference.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import AgoraUIKit from 'agora-rn-uikit';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Conference(props) {
+  const navigation = useNavigation();
+
   const rtcProps = {
     appId: 'c7e742d5df23478285a9dc4f4ff62407',
     channel: props.route.params.channel,
   };
-  return <AgoraUIKit rtcProps={rtcProps} />;
+
+  const callbacks = {
+    EndCall: () => navigation.goBack(),
+  };
+
+  return <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />;
 }

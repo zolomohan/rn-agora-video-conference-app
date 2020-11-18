735a748d3434e50bbdf8735ff694052371cd9282
zolomohan <mohanzolomon@gmail.com>
2020-11-19 03:17:27 +0530

    Add Agora UI Kit

--- screens/Conference.js
+++ screens/Conference.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AgoraUIKit from 'agora-rn-uikit';
 
-export default function Conference() {
-  return (
-    <View style={styles.container}>
-      <Text>Conference</Text>
-    </View>
-  );
+export default function Conference(props) {
+  const rtcProps = {
+    appId: 'c7e742d5df23478285a9dc4f4ff62407',
+    channel: props.route.params.channel,
+  };
+  return <AgoraUIKit rtcProps={rtcProps} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
